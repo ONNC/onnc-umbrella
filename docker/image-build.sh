8f6b4526c8d129bd1779c3d0a241567a038426d6
Eric Yu <eric.yu@iclink.tw>
2018-04-25 17:20:12 +0800

    new file:   Dockerfile.dev

--- docker/image-build.sh
+++ docker/image-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 ver=1.1
-docker build -t iclink/onnc_dev:$ver -f Dockerfile.base .
+docker build -t iclink/onnc_dev:$ver -f Dockerfile.dev .
 docker push iclink/onnc_dev:$ver
 docker tag iclink/onnc_dev:$ver iclink/onnc_dev:latest
 docker push iclink/onnc_dev:latest
