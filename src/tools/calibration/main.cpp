3efe0ecc77cf4fc3eb249a7c41c6bc7d95b09440
zakk.chen <zakk.chen@iclink.tw>
2018-05-07 17:53:49 +0000

    add ONNCModulePrinter pass

---
+++ src/tools/calibration/main.cpp
@@ -0,0 +1,35 @@
+//===- PassManagerTest.h --------------------------------------------------===//
+//
+//                             The ONNC Project
+//
+// See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <onnc/Core/PassManager.h>
+#include <onnc/IR/Module.h>
+#include <onnc/IR/ONNCModulePrinter.h>
+#include <onnc/IRReader/ONNXReader.h>
+
+using namespace onnc;
+
+int main(int pArgc, char *pArgv[])
+{
+  if (pArgc != 2) {
+    std::cerr << "usage:  " << pArgv[0] << " onnx_file" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  onnc::onnx::Reader reader;
+  onnc::SystemError err;
+  std::unique_ptr<onnc::Module> module(reader.parse(onnc::Path(pArgv[1]), err));
+  if (!err.isGood()) {
+    return EXIT_FAILURE;
+  }
+  PassManager pm;
+  pm.add(createONNCModulePrinterPass());
+  pm.run(*module);
+}
