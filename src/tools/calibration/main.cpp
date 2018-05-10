bc616ec063104c419e6818a9f7e7766e1ab8b2dc
zakk.chen <zakk.chen@iclink.tw>
2018-05-10 04:03:15 +0000

    refine calibration main.cpp

--- src/tools/calibration/main.cpp
+++ src/tools/calibration/main.cpp
@@ -28,42 +28,47 @@ int main(int pArgc, char *pArgv[])
   onnc::onnx::Reader reader;
   onnc::SystemError err;
   std::unique_ptr<onnc::Module> module(reader.parse(onnc::Path(pArgv[1]), err));
-  if (!err.isGood()) {
-    return EXIT_FAILURE;
+  {
+    if (!err.isGood()) {
+      return EXIT_FAILURE;
+    }
+    onnc::PassManager pm;
+    pm.add(::onnc::createONNCModulePrinterPass());
+    pm.add(::onnc::createInsertDummyCtablePass());
+    pm.run(*module);
   }
-  onnc::PassManager pm;
-  pm.add(::onnc::createONNCModulePrinterPass());
-  pm.add(::onnc::createInsertDummyCtablePass());
-  pm.run(*module);
 
   // FIXME add ONNXIRWritter
   // write the new onnx model back to disk
   const char *fileName = "new.onnx";
-  ::onnx::ModelProto modelProto;
-  // init IR version
-  modelProto.set_ir_version(3);
-  // init graph
-  ::onnx::ExportModelProto(&modelProto, module->getGraphIR());
-  // init metadata
-  for (auto const &data : module->getMetaData()) {
-    auto *metadata_props = modelProto.add_metadata_props();
-    metadata_props->set_key(data.first);
-    metadata_props->set_value(data.second);
-  }
   {
+    ::onnx::ModelProto modelProto;
+    // init IR version
+    modelProto.set_ir_version(3);
+    // init graph
+    ::onnx::ExportModelProto(&modelProto, module->getGraphIR());
+    // init metadata
+    for (auto const &data : module->getMetaData()) {
+      auto *metadata_props = modelProto.add_metadata_props();
+      metadata_props->set_key(data.first);
+      metadata_props->set_value(data.second);
+    }
+    // wrtie file
     std::fstream output(fileName,
                         std::ios::out | std::ios::trunc | std::ios::binary);
     modelProto.SerializeToOstream(&output);
   }
 
-  // dump new onnx model
-  std::cout << "after InsertDummyCtable" << std::endl;
-  std::unique_ptr<onnc::Module> module2(
-      reader.parse(onnc::Path(fileName), err));
-  if (!err.isGood()) {
-    return EXIT_FAILURE;
+  // read and print new onnx model
+  {
+    std::cout << "after InsertDummyCtable" << std::endl;
+    std::unique_ptr<onnc::Module> module2(
+        reader.parse(onnc::Path(fileName), err));
+    if (!err.isGood()) {
+      return EXIT_FAILURE;
+    }
+    ::onnc::PassManager pm;
+    pm.add(::onnc::createONNCModulePrinterPass());
+    pm.run(*module);
   }
-  ::onnc::PassManager pm2;
-  pm2.add(::onnc::createONNCModulePrinterPass());
-  pm2.run(*module2);
 }
