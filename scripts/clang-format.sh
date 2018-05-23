517b023d517bf9b73c1b298d6622b90676d8a21e
Eric Yu <eric.yu@iclink.tw>
2018-05-23 15:20:17 +0800

    avoid clang-format run on merged commit

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -3,7 +3,14 @@
 dir=`dirname $0`
 cd $dir/..
 
+if git show HEAD^2 >& /dev/null
+then 
+    echo "git diff does not work with git merge"
+    echo "clang-format abort"
+    exit
+fi
+
 git diff --exit-code
-git diff -U0 HEAD^ | clang-format-diff.py -p1 -i
-git diff -U0 HEAD^ | clang-tidy-diff.py -path build -p1 -fix
+git diff -U0 HEAD~$@ | clang-format-diff.py -p1 -i
+git diff -U0 HEAD~$@ | clang-tidy-diff.py -path build -p1 -fix
 git diff --exit-code
