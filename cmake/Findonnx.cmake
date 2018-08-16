fa67239889e3c6c365def9c24e523c06599072cd
LuisHsu <luishsuplus@gmail.com>
2018-08-16 14:27:08 +0800

    Remove all CMake file and use shell script instead

--- cmake/Findonnx.cmake
+++
@@ -1,9 +0,0 @@
-find_library(ONNX_LIBRARIES
-    NAMES onnx
-)
-get_filename_component(ONNX_LIBRARY_DIR ${ONNX_LIBRARIES} DIRECTORY)
-find_path(ONNX_INCLUDE_DIR
-    NAMES onnx/onnx.pb.h
-)
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(ONNX DEFAULT_MSG ONNX_INCLUDE_DIR ONNX_LIBRARY_DIR)
