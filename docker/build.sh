3d7dde0a0c82c4c1a580d89cd0b185806e9dc723
TDYa127 <a127a127@skymizer.com>
2018-10-01 07:18:18 +0800

    Reorder ARG to reuse cache. Fix docker build.

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 #                       The ONNC Project
 
-docker build -t onnc-dev-debug --build-arg MODE=debug -f docker/Dockerfile .
+set -ex
+
+docker build -t onnc-dev-debug --build-arg MODE=dbg -f docker/Dockerfile .
 docker build -t onnc-dev-normal --build-arg MODE=normal -f docker/Dockerfile .
