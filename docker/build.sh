097f142569918c43c49259cbfca1c99374aff7bf
TDYa127 <a127a127@skymizer.com>
2018-10-03 17:00:09 +0800

    [docker] Separate external-prebuilt. "/home/onnc" -> "onnc".

--- docker/build.sh
+++ docker/build.sh
@@ -3,5 +3,7 @@
 
 set -ex
 
+docker build -t onnc-dev-external-prebuilt -f docker/Dockerfile.external-prebuilt .
+
 docker build -t onnc-dev-debug --build-arg MODE=dbg -f docker/Dockerfile .
 docker build -t onnc-dev-normal --build-arg MODE=normal -f docker/Dockerfile .
