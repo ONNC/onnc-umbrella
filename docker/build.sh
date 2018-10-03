609a6b085d745233ab95914c711450cb588ca4b6
TDYa127 <a127a127@skymizer.com>
2018-10-03 19:16:56 +0800

    [docker] Fast build infrastructure.

--- docker/build.sh
+++ docker/build.sh
@@ -5,5 +5,5 @@ set -ex
 
 docker build -t onnc/onnc:external-prebuilt -f docker/Dockerfile.external-prebuilt .
 
-docker build -t onnc/onnc-debug --build-arg MODE=dbg -f docker/Dockerfile .
 docker build -t onnc/onnc --build-arg MODE=normal -f docker/Dockerfile .
+docker build -t onnc/onnc-debug --build-arg MODE=dbg -f docker/Dockerfile .
