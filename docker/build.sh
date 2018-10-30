d87d7fc1dc5fb0e46711bcd43f651bcf94bb089c
TDYa127 <a127a127@skymizer.com>
2018-10-30 10:46:07 +0800

    [docker] Add release script.

--- docker/build.sh
+++ docker/build.sh
@@ -3,7 +3,7 @@
 
 set -ex
 
-docker build -t onnc/onnc:external-prebuilt -f docker/Dockerfile.external-prebuilt .
+docker build -t onnc/onnc-dev-external-prebuilt -f docker/Dockerfile.external-prebuilt .
 
-docker build -t onnc/onnc --build-arg MODE=normal -f docker/Dockerfile .
-docker build -t onnc/onnc-debug --build-arg MODE=dbg -f docker/Dockerfile .
+docker build -t onnc/onnc-dev-normal --build-arg MODE=normal -f docker/Dockerfile .
+docker build -t onnc/onnc-dev-debug --build-arg MODE=dbg -f docker/Dockerfile .
---
+++ docker/release.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+PUSH=
+NO_REBUILD=
+TAG="latest"
+
+function help_info() {
+  echo "Usage: release.sh [options]... [TAG]"
+  echo "    --push              Push to docker hub after tag images."
+  echo "    --no-rebuild        Do not rebuild images before tag."
+  echo "    TAG                 Release tag name (default: \"latest\")"
+}
+
+#----------------------------------------------------------
+#   Arguments parsing
+#----------------------------------------------------------
+while [[ $# -gt 0 ]];
+do
+  key="$1"
+  shift
+  case $key in
+    -h|--help)
+      help_info
+      exit 0
+      ;;
+    --push)
+      PUSH=1
+      ;;
+    --no-rebuild)
+      NO_REBUILD=1
+      ;;
+    *)
+      "$key"
+      ;;
+  esac
+done
+
+if [ -z "$NO_REBUILD" ]; then
+  echo "== Rebuild images for release =="
+  "$SCRIPT_DIR/build.sh"
+fi
+
+echo "== Tag images for release =="
+docker tag onnc/onnc-dev-external-prebuilt "onnc/onnc-dev-external-prebuilt:$TAG"
+docker tag onnc/onnc-dev-normal "onnc/onnc-normal:$TAG"
+docker tag onnc/onnc-dev-debug "onnc/onnc-debug:$TAG"
+
+if [ -n "$PUSH" ]; then
+  echo "== Tag images for release =="
+  docker push "onnc/onnc-dev-external-prebuilt:$TAG"
+  docker push "onnc/onnc-normal:$TAG"
+  docker push "onnc/onnc-debug:$TAG"
+fi
