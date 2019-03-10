8526d104ddae13c3cdaa00b47fe549ac8e5ab331
Po Yen Chen <poyenc@skymizer.com>
2019-03-11 07:56:33 +0800

    Only build necessary docker images

--- docker/build.sh
+++ docker/build.sh
@@ -6,5 +6,3 @@ set -ex
 docker build -t onnc/onnc-dev-external-prebuilt -f docker/Dockerfile.external-prebuilt .
 
 docker build -t onnc/onnc-community -f docker/Dockerfile.community .
-
-docker build -t onnc/onnc-community-vp -f docker/Dockerfile.community-vp .
