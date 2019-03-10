92ff8f162ac9d57cfec17139dfc0bcaf5266e06e
Po Yen Chen <poyenc@skymizer.com>
2019-03-11 00:50:56 +0800

    Move golden NVDLA loadables into onnc-community docker image

--- docker/build.sh
+++ docker/build.sh
@@ -7,5 +7,4 @@ docker build -t onnc/onnc-dev-external-prebuilt -f docker/Dockerfile.external-pr
 
 docker build -t onnc/onnc-community -f docker/Dockerfile.community .
 
-./docker/build-nvdla-loadables.sh
 docker build -t onnc/onnc-community-vp -f docker/Dockerfile.community-vp .
